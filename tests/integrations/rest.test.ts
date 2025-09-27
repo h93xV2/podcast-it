6566c780011a566368dc49a3be0959286497418b
h93xV2 <h93xV2@users.noreply.github.com>
2025-09-27 12:04:03 -0700

    Fix broken unit test

--- tests/integrations/rest.test.ts
+++ tests/integrations/rest.test.ts
@@ -174,7 +174,9 @@ describe("Episode creation", () => {
     );
 
     expect(secondCreateResponse.status).toBe(409);
-    expect(await secondCreateResponse.json()).toEqual({message: "Conflict: episode already exists"});
+    expect(await secondCreateResponse.json()).toEqual({
+      message: "Conflict: episode already exists",
+    });
   });
 
   it("returns 400 when the wrong arguments are passed", async () => {
